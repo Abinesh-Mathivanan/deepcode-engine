72c9331091af950da6ee90c22bc4373437071d9b
Beens <abineshmathivanan31@gmail.com>
2025-04-15 20:25:10 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.v1.code_execution import router as r
 
 app = FastAPI()
+
+# Add CORS middleware to allow requests from localhost:3000
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow requests from localhost:3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 app.include_router(r, prefix="/api/v1")
